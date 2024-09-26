fbc37667fc0d3b42c11ffc80398a4866dfc82fe5
Student <tierney@rcc.team>
2024-09-26 10:22:27 -0400

    Shopping List revised

--- src/test/java/com/pluralsight/ShoppingList.java
+++ src/test/java/com/pluralsight/ShoppingList.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
 public class ShoppingList {
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Shopping list:");
+        System.out.println("-------------------");
+        System.out.println("Apples");
+        System.out.println("Olives");
+        System.out.println("Cashews");
+        System.out.println("Soda");
+        System.out.println("Chicken");
+        System.out.println("Chips");
+    }
 }
